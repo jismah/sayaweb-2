23440d1d0ef204d33d63acddaa18ef23869ccf1c
jismah <ji_rq019@hotmail.com>
2023-08-29 08:55:01 -0400

    Profile Users Done

--- src/pages/App/Users/index.tsx
+++ src/pages/App/Users/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure, Text, Avatar } from "@chakra-ui/react";
 import { PlusIcon } from "@heroicons/react/20/solid";
 import { Card, Table, MultiSelect, MultiSelectItem, TableBody, TableCell, TableHead, TableHeaderCell, TableRow, Button } from "@tremor/react";
 import { NextPage } from "next";
@@ -19,6 +19,8 @@ const UsersPage: NextPage = () => {
     const [listUsers, setListUsers] = useState<ProfileUser[]>([]);
     const [selectedNames, setSelectedNames] = useState<string[]>([]);
 
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
     const isUserSelected = (user: ProfileUser) =>
         selectedNames.includes(user.username) || selectedNames.length === 0;
 
@@ -28,7 +30,7 @@ const UsersPage: NextPage = () => {
             .from('profiles')
             .select();
 
-        
+
         if (data) {
             setListUsers(data);
         }
@@ -46,7 +48,7 @@ const UsersPage: NextPage = () => {
             <Box p={4} width={'100%'}>
                 <Flex justifyContent={"space-between"} alignItems={"center"}>
                     <Heading size={"md"}>Usuarios</Heading>
-                    <Button size="xs" color="teal" icon={PlusIcon}>Crear Usuario</Button>
+                    <Button size="xs" color="teal" icon={PlusIcon} disabled={true}>Crear Usuario</Button>
 
                 </Flex>
 
@@ -81,11 +83,50 @@ const UsersPage: NextPage = () => {
                                         <TableCell>{item.username}</TableCell>
                                         <TableCell>{item.full_name}</TableCell>
                                         <TableCell className="text-right">
-                                            <Button size="xs" variant="secondary" color="teal">
+                                            <Button size="xs" variant="secondary" color="teal" onClick={onOpen}>
                                                 Ver Detalles
                                             </Button>
+                                            <Modal isOpen={isOpen} onClose={onClose} isCentered>
+                                                <ModalOverlay />
+                                                <ModalContent>
+                                                    <ModalHeader>Usuario Detalles</ModalHeader>
+                                                    <ModalCloseButton />
+                                                    <ModalBody>
+                                                        <Flex alignItems={"center"} mb={5}>
+                                                            <Avatar name={item.username} src={item.avatar_url} />
+                                                            <Text fontWeight='bold' ml={2}>{item.username}</Text>
+                                                        </Flex>
+                                                        <Box mb={3}>
+                                                            <Heading size={"sm"}>
+                                                                ID
+                                                            </Heading>
+                                                            <Text>
+                                                                {item.id}
+                                                            </Text>
+                                                        </Box>
+                                                        <Box>
+                                                            <Heading size={"sm"}>
+                                                                Nombre Completo
+                                                            </Heading>
+                                                            <Text>
+                                                                {item.full_name}
+                                                            </Text>
+                                                        </Box>
+                                                    </ModalBody>
+
+                                                    <ModalFooter>
+                                                        <Button color='teal' onClick={onClose}>
+                                                            Cerrar
+                                                        </Button>
+                                                    </ModalFooter>
+                                                </ModalContent>
+                                            </Modal>
                                         </TableCell>
+
+
                                     </TableRow>
+
+
                                 ))}
                         </TableBody>
                     </Table>
